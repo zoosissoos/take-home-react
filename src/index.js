054b8672b4615d7066f899fae9dd55147a256db0
Dan Lewis <danlewis@Dans-MacBook-Air.local>
2021-11-11 22:59:37 -0500

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,84 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function firstLetterUppercase (str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+async function fetchData (url) {
+  if(url) {
+    try{
+      const res = await fetch(url)
+      return res.json()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+
+
+function DataCol({title, data}) {
+  const styles = {
+    col: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-start'
+    }
+  }
+  return (
+    <div styles={styles.col}>
+      <h2>{title}</h2>
+      {data ? data.map(({name}) => {
+        const formattedName = firstLetterUppercase(name)
+        return <p>{formattedName}</p>
+      }): null}
+    </div>
+  )
+}
+
+
+function App() {
+  const [data, setData] = useState(null)
+
+  async function gatherData() {
+    const users = await fetchData('https://jsonplaceholder.typicode.com/users')
+    const { results } = await fetchData('https://pokeapi.co/api/v2/pokemon-species/')
+    return ({users, pokemon: results})
+  }
+
+  useEffect(() => {
+    gatherData().then((data) => setData(data))
+  }, [])
+
+  const styles = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      border: '1px solid black',
+      padding: '1rem',
+      borderRadius: '6px',
+      background: 'ivory',
+      flexWrap: 'wrap',
+      textAlign: 'center'
+    },
+    row: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      textAlign: 'left'
+    }
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1>Users & Pokemon</h1>
+      <div style={styles.row}>
+        <DataCol title='Users' data={data?.users || null}/>
+        <DataCol title='Pokemon' data={data?.pokemon || null}/>
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +86,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
